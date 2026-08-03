1d56c3717a00b86d422f2b544e282f74685179d9
jjoaovvic <jjoao.vvic@gmail.com>
2026-08-03 17:03:36 -0300

    Player_Movement

---
+++ navinha/scripts/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+const BULLET_TIME = 0.2
+var can_shoot = 1
+
+func _ready() -> void:
+	%Bullet_Timer.wait_time = BULLET_TIME
+
+func get_input():
+	var direction := Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED
+
+func shoot():
+	const BULLET = preload("res://prefabs/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_transform = %Gun.global_transform
+	new_bullet.global_rotation = %Gun.global_rotation
+	add_child(new_bullet)
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("shoot") and can_shoot == 1:
+		shoot()
+		can_shoot = 0
+		%Bullet_Timer.start()
+
+func _on_bullet_time_timeout() -> void:
+	can_shoot = 1
