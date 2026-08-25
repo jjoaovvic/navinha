8715ec430aac1e48cb9f770c3b327dfd380d9ed3
bench <jictyvoo.ecomp@gmail.com>
2026-08-25 14:46:33 -0300

    :file_cabinet: refactor(ui): Move menu and upgrade into ui/

--- navinha/scripts/world.gd
+++ navinha/scripts/world.gd
@@ -34,7 +34,7 @@ func _on_upgrade_button_pressed(button_path:NodePath) -> void:
 
 func _on_menu_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
 
 func _on_restart_button_pressed() -> void:
 	get_tree().paused = false
