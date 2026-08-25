5b42a12260c2fc68f8558c57cfb1753f15df4ffb
bench <jictyvoo.ecomp@gmail.com>
2026-08-25 14:46:33 -0300

    :file_folder: refactor(world): Move the main scene into world/

--- navinha/ui/main_menu/main_menu.gd
+++ navinha/ui/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/test.tscn")
+	get_tree().change_scene_to_file("res://world/world.tscn")
 
 
 func _on_exit_button_pressed() -> void:
