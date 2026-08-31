1e70704339d5611cc99ff279783edf47ddebd565
jjoaovvic <jjoao.vvic@gmail.com>
2026-08-31 10:57:17 -0300

    Random Enemies
    Adicionado gerador aleatório de inimigos e timer para o spawner

--- navinha/entities/spawner/spawner.gd
+++ navinha/entities/spawner/spawner.gd
@@ -1,9 +1,21 @@
 extends Sprite2D
 
 @export var load_enemy : PackedScene
+var pursuer = preload("res://entities/enemies/pursuer/pursuer.tscn")
+var shooter = preload("res://entities/enemies/shooter/shooter.tscn")
+@onready var timer = %SpawnTime
+
 
 func spawn():
-	var enemy = load_enemy.instantiate()
+	timer.start()
+	await timer.timeout
+	var enemy : Node2D
+	if load_enemy == null:
+		var random_enemy = randi() % 2
+		var enemies = [pursuer, shooter]
+		enemy = enemies[random_enemy].instantiate()
+	else:
+		enemy = load_enemy.instantiate()
 	enemy.global_position = global_position
 	add_sibling(enemy)
 	queue_free()
