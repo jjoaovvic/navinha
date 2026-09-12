a650767d456fb7b87f547dedfdae8f268163dd7a
João Victor Oliveira Couto <commits@jictyvoo.dev>
2026-09-12 15:40:58 -0300

    :wrench: feat(spawner): Generalize the enemy pool into an array

--- navinha/entities/spawner/spawner.gd
+++ navinha/entities/spawner/spawner.gd
@@ -1,19 +1,25 @@
 extends Sprite2D
 
 @export var load_enemy : PackedScene
-var pursuer = preload("res://entities/enemies/pursuer/pursuer.tscn")
-var shooter = preload("res://entities/enemies/shooter/shooter.tscn")
-@onready var timer = %SpawnTime
+@onready var timer : Timer = %SpawnTime
 
+@onready var pursuer : PackedScene
+@onready var shooter : PackedScene
+
+var enemy_pool : Array[PackedScene] = []
+
+func _ready() -> void:
+	pursuer = preload("res://entities/enemies/pursuer/pursuer.tscn")
+	shooter = preload("res://entities/enemies/shooter/shooter.tscn")
+	enemy_pool = [pursuer, shooter]
 
 func spawn():
 	timer.start()
 	await timer.timeout
 	var enemy : Node2D
 	if load_enemy == null:
-		var random_enemy = randi() % 2
-		var enemies = [pursuer, shooter]
-		enemy = enemies[random_enemy].instantiate()
+		var random_index = randi() % enemy_pool.size()
+		enemy = enemy_pool[random_index].instantiate()
 	else:
 		enemy = load_enemy.instantiate()
 	enemy.global_position = global_position
